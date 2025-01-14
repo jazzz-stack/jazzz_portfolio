baf78c5239beb0cb55048a40e7d7492ed9b544c1
JasvantRaj <jasvant.raj@centricconsulting.com>
2025-01-14 14:26:55 +0530

    Update base path to /portfolio/

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/testcicd/", // Add this line
+  base: "/portfolio/", // Updated line
   plugins: [react()],
 });

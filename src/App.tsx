4e0b7024786d51d457d87037b90750845f340ae4
JasvantRaj <jasvant.raj@centricconsulting.com>
2025-01-14 21:27:27 +0530

    updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,140 @@ function App() {
       <section className="about">
         <h2>About Me</h2>
         <p>
-          Hi, I'm [Your Name], a passionate software developer with experience
+          Hi, I'm Jaswant Raj, a passionate software developer with experience
           in building web applications.
         </p>
+        <p>
+          <strong>Contact:</strong> +91-8052004200 |{" "}
+          <a href="mailto:jaswant.raj45@gmail.com">jaswant.raj45@gmail.com</a>
+        </p>
+        <p>
+          <strong>LinkedIn:</strong>{" "}
+          <a
+            href="https://linkedin.com/in/jaswant-raj"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin.com/in/jaswant-raj
+          </a>
+        </p>
+        <p>Location: Lucknow, Uttar Pradesh, India, 226016</p>
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>React Native</li>
+          <li>Reactjs</li>
+          <li>JavaScript & ES6</li>
+          <li>Jest</li>
+          <li>JSX</li>
+          <li>CSS</li>
+          <li>TypeScript</li>
+          <li>RESTful API's</li>
+          <li>Redux</li>
+          <li>GitHub</li>
+          <li>Azure DevOps</li>
+          <li>Socket API</li>
+          <li>Redux-toolkit</li>
+          <li>GraphQL</li>
+          <li>AppDynamics</li>
+          <li>Git</li>
+          <li>ServiceNow</li>
+          <li>React-Bootstrap</li>
+          <li>App Store & Play Store</li>
+          <li>Full Story</li>
+        </ul>
       </section>
       <section className="experience">
         <h2>Experience</h2>
         <ul>
           <li>
-            <h3>Company A</h3>
-            <p>Role: Software Engineer</p>
-            <p>Duration: Jan 2020 - Present</p>
-            <p>
-              Responsibilities: Developing web applications using React and
-              Node.js.
-            </p>
+            <h3>Senior Software Engineer - Centric Consulting</h3>
+            <p>07/2022 - Present, Gurugram</p>
+            <ul>
+              <li>
+                Established Centric Consulting as a leader in AI strategy, cyber
+                risk management, and cloud solutions, driving a 40% increase in
+                client acquisition, 25% revenue growth, and expanding into 15
+                new markets.
+              </li>
+              <li>
+                Implemented 100+ new User Stories, addressing critical bugs and
+                leading unit testing efforts which resulted in a 20% reduction
+                in app crashes and improved user experience metrics by 15%.
+              </li>
+              <li>
+                Trained support engineers, achieving a 30% improvement in
+                troubleshooting efficiency.
+              </li>
+            </ul>
           </li>
           <li>
-            <h3>Company B</h3>
-            <p>Role: Frontend Developer</p>
-            <p>Duration: Jun 2018 - Dec 2019</p>
-            <p>
-              Responsibilities: Building responsive user interfaces with HTML,
-              CSS, and JavaScript.
-            </p>
+            <h3>React Native Developer - Suffescom Solutions Pvt.</h3>
+            <p>12/2021 - 06/2022, Chandigarh</p>
+            <ul>
+              <li>
+                Cloned 6+ projects and added new features and functionality.
+              </li>
+              <li>
+                Transformed the JavaScript code to TypeScript, increasing code
+                quality by 40-50%.
+              </li>
+              <li>
+                Published 4+ Applications on the app store and Play Store
+                successfully.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>React Native & ReactJs Developer - Coding Brains</h3>
+            <p>06/2021 - 12/2021, Lucknow</p>
+            <ul>
+              <li>
+                Maintained regular client communication, implemented innovative
+                features, committed code on GitHub, and ensured 90% unit test
+                coverage.
+              </li>
+              <li>
+                Analyzed design specs and documentation, improving product and
+                code quality by 20%. Migrated React web apps to React Native,
+                enhancing development efficiency by 25% and reducing bugs by
+                40%.
+              </li>
+              <li>
+                Worked with a web team that increased productivity by 40%.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>React Native Developer - Infranix Technologies</h3>
+            <p>02/2020 - 06/2021, Lucknow</p>
+            <ul>
+              <li>
+                Created streamlined code templates, enhancing integration,
+                increasing coding efficiency by 10%, and reducing code review
+                times by 20%.
+              </li>
+              <li>
+                Integrated In-App Purchase features for 200+ videos on both
+                platforms.
+              </li>
+              <li>
+                Created and tested 10+ apps for cross-platform within 6 months
+                of training.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>Intern- Software Engineer - Infowiz Software Solution</h3>
+            <p>02/2019 - 06/2019, Chandigarh</p>
+            <ul>
+              <li>
+                Infowiz is a north India’s leading Industrial Training company
+                and has evolved into a trustworthy brand in the training
+                industry.
+              </li>
+            </ul>
           </li>
         </ul>
       </section>
@@ -40,33 +150,97 @@ function App() {
         <h2>Projects</h2>
         <ul>
           <li>
-            <h3>Project A</h3>
-            <p>Description: A web application for managing tasks.</p>
-            <p>Technologies: React, Node.js, MongoDB</p>
-            <a
-              href="https://github.com/yourusername/project-a"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on GitHub
-            </a>
+            <h3>PepsiCo - MiNegocio+</h3>
+            <p>04/2022 - Present, USA</p>
+            <ul>
+              <li>
+                Enhanced digital ordering and profile management with OKTA
+                integration, increasing order accuracy by 40% and customer
+                satisfaction by 25%.
+              </li>
+              <li>
+                Provided 6+ trainings to troubleshoot the bugs & crash issues.
+              </li>
+            </ul>
           </li>
           <li>
-            <h3>Project B</h3>
-            <p>Description: A mobile app for tracking fitness activities.</p>
-            <p>Technologies: React Native, Firebase</p>
-            <a
-              href="https://github.com/yourusername/project-b"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on GitHub
-            </a>
+            <h3>AER Taxi</h3>
+            <p>03/2022 - 06/2022, Chandigarh</p>
+            <ul>
+              <li>
+                Cloned 6+ projects, implementing new features, bug fixing, and
+                publishing apps on stores.
+              </li>
+              <li>
+                Led the team in writing unit tests, improving code quality and
+                productivity by 60%.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>eComNow</h3>
+            <p>06/2021 - 08/2021, Lucknow</p>
+            <ul>
+              <li>
+                Led the mobile and web team to achieve the goal using React &
+                React Native technology.
+              </li>
+              <li>
+                The outcome of the successful delivery was 20% of company
+                growth.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h3>Box Pro fitness</h3>
+            <p>12/2020 - 02/2021, Lucknow</p>
+            <ul>
+              <li>
+                Implemented all the requirements by myself and fixed bugs.
+              </li>
+              <li>
+                Delivered the product to the client successfully, causing the
+                client to be very happy, and he gave a 10% bonus amount to the
+                company.
+              </li>
+              <li>
+                The target deadline was 6 months, but I delivered the project
+                within 3 months.
+              </li>
+            </ul>
           </li>
         </ul>
       </section>
+      <section className="education">
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <h3>Master of Computer Application - Chandigarh University</h3>
+            <p>12/2017 - 05/2019</p>
+          </li>
+          <li>
+            <h3>
+              Bachelor of Computer Application - Makhanlal Chaturvedi National
+              University
+            </h3>
+            <p>12/2013 - 06/2016</p>
+          </li>
+        </ul>
+      </section>
+      <section className="achievements">
+        <h2>Achievements</h2>
+        <ul>
+          <li>
+            Employee of the year award - Award for performing well in the team.
+          </li>
+          <li>Silver medal - A marathon race competition medal.</li>
+        </ul>
+      </section>
       <footer>
-        <p>Contact: jaswant.raj45@gmail.com</p>
+        <p>
+          Contact:{" "}
+          <a href="mailto:jaswant.raj45@gmail.com">jaswant.raj45@gmail.com</a>
+        </p>
       </footer>
     </div>
   );

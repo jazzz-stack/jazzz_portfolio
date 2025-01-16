2960f09263f9f7dbb2046ec8fc5a11d30eabafe6
JasvantRaj <jasvant.raj@centricconsulting.com>
2025-01-16 09:47:09 +0530

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,7 @@ import Projects from "../components/Projects";
 import Intro from "../components/Intro";
 
 function About() {
-  const contentRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<any>(null);
 
   const handlePrint = useReactToPrint({ contentRef });
 

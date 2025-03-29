0da8894419c530c344f6d70170db6d49dc0a85e9
JasvantRaj <jasvant.raj@centricconsulting.com>
2025-03-29 13:27:39 +0530

    add Intro component with personal and professional details

---
+++ src/components/Intro.tsx
@@ -0,0 +1,57 @@
+function Intro() {
+  return (
+    <section className="mb-8">
+      <h2 className="text-2xl font-bold border-b-2 border-gray-800 pb-2 mb-4">
+        About Me
+      </h2>
+      <p className="mb-4">
+        I am a skilled Senior Software Engineer with extensive experience in
+        developing, enhancing, and optimizing mobile and web applications. My
+        expertise includes React Native, React.js, JavaScript, TypeScript,
+        RESTful APIs, and modern development tools like Redux, Jest, vitest, and
+        Git.
+      </p>
+      <p className="mb-4">
+        With over 4 years of professional experience, I have contributed to the
+        success of projects across multiple domains, achieving significant
+        results such as a 70-80% increase in app smoothness, improved code
+        quality by 40-50%, and a 20% reduction in app crashes through meticulous
+        debugging and code refactoring. I have successfully led the
+        implementation of key features like On-Prem user accounts, in-app
+        purchases, and cross-platform functionalities.
+      </p>
+      <p className="mb-4">
+        In my current role as a Senior Software Engineer at Centric Consulting,
+        I have driven a 40% increase in client acquisition and a 25% revenue
+        growth by spearheading initiatives in AI strategy, cyber risk
+        management, and cloud solutions. I am passionate about optimizing
+        performance, ensuring seamless user experiences, and delivering
+        high-quality products that exceed client expectations.
+      </p>
+      <h2 className="text-2xl font-bold border-b-2 border-gray-800 pb-2 mb-4">
+        Key Highlights
+      </h2>
+      <ul className="list-disc list-inside ml-4">
+        <li>
+          Published 20+ applications on app stores with exceptional quality.
+        </li>
+        <li>
+          Improved development efficiency by 60% and reduced production bugs
+          significantly through rigorous unit testing.
+        </li>
+        <li>
+          Recognized as "Employee of the Year" for outstanding contributions to
+          team performance and project success.
+        </li>
+        <li>
+          I am committed to leveraging my technical expertise and
+          problem-solving skills to drive innovation and deliver impactful
+          solutions. Let’s connect to discuss how I can contribute to your
+          team’s success.
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Intro;
